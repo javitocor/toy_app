87b1122aed2ab53385c82d1181001e31e8774240
javitocor <javitocor@gmail.com>
2020-04-08 10:53:31 +0200

    Added microposts feature, input validation and display first post for every user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+    has_many :microposts
+    validates :name, presence: true
+    validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i },
+                      presence: true
 end
